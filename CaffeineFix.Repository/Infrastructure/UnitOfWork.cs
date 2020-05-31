ef76e1b2c03b727267e7f088bc39e013974a0745
Jairus Macatangay <jairus.macatangay@benilde.edu.ph>
2020-05-31 13:26:15 +0800

    Initial Files

---
+++ CaffeineFix.Repository/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaffeineFix.Repository.Infrastructure.Contract;
+
+namespace CaffeineFix.Repository.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CaffeineFixEntities _dbContext;
+
+        public UnitOfWork()
+        {
+            _dbContext = new CaffeineFixEntities();
+        }
+
+        public DbContext Db
+        {
+            get { return _dbContext; }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ CaffeineFix/Infrastructure/AutomapperWebProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaffeineFix.Infrastructure
+{
+    public class AutomapperWebProfile : AutoMapper.Profile
+    {
+        public AutomapperWebProfile()
+        {
+
+        }
+
+        public static void Run()
+        {
+            AutoMapper.Mapper.Initialize(a =>
+            {
+                a.AddProfile<AutomapperWebProfile>();
+            });
+        }
+    }
+}
